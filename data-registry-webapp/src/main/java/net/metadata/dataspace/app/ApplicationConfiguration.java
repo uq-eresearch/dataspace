bf860f6a817e8d884ba403a6dbd8769760fdf910
Abdul Alabri <uqaalabr@uq.edu.au>
2010-09-13 06:25:54 +0000

    Initial project setup.
    git-svn-id: https://source.eait.uq.edu.au/svn/maenad-data-registry/trunk@1 a06b0660-b68b-0410-9a37-e6c9a29c9ca9

---
+++ data-registry-webapp/src/main/java/net/metadata/dataspace/app/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package net.metadata.dataspace.app;
+
+/**
+ * User: alabri
+ * Date: 13/09/2010
+ * Time: 3:34:49 PM
+ */
+public interface ApplicationConfiguration {
+}
---
+++ data-registry-webapp/src/main/java/net/metadata/dataspace/app/ApplicationContext.java
@@ -0,0 +1,10 @@
+package net.metadata.dataspace.app;
+
+/**
+ * User: alabri
+ * Date: 13/09/2010
+ * Time: 3:37:39 PM
+ */
+public class ApplicationContext implements ApplicationConfiguration {
+
+}
