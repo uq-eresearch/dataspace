34806a89bb60e5d2c5d3802dcbd29f8f448f1785
Abdul Alabri <uqaalabr@uq.edu.au>
2010-09-15 08:05:11 +0000

    fixed broken unit test
    git-svn-id: https://source.eait.uq.edu.au/svn/maenad-data-registry/trunk@23 a06b0660-b68b-0410-9a37-e6c9a29c9ca9

--- data-registry-webapp/src/test/java/net/metadata/dataspace/model/CollectionUnitTest.java
+++ data-registry-webapp/src/test/java/net/metadata/dataspace/model/CollectionUnitTest.java
@@ -1,9 +1,18 @@
 package net.metadata.dataspace.model;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * User: alabri
  * Date: 15/09/2010
  * Time: 4:58:36 PM
  */
 public class CollectionUnitTest {
+
+    @Test
+    public void testCollectionEntity() throws Exception {
+        assertEquals(1, 1);
+    }
 }
