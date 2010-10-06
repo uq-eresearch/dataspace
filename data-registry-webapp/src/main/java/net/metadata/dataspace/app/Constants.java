4778e3a833cadf37ee8b6a964d8d8cef65a59a32
Abdul Alabri <uqaalabr@uq.edu.au>
2010-10-06 01:37:42 +0000

    Added constants class for system wide use
    git-svn-id: https://source.eait.uq.edu.au/svn/maenad-data-registry/trunk@64 a06b0660-b68b-0410-9a37-e6c9a29c9ca9

---
+++ data-registry-webapp/src/main/java/net/metadata/dataspace/app/Constants.java
@@ -0,0 +1,13 @@
+package net.metadata.dataspace.app;
+
+/**
+ * User: alabri
+ * Date: 06/10/2010
+ * Time: 8:16:32 AM
+ */
+public interface Constants {
+
+    String JSON_MIMETYPE = "application/json";
+
+    String ATOM_MIMETYPE = "application/atom+xml";
+}
