945cf3081b6f0cfdd58decd41f2d70d69ad8075f
Abdul Alabri <uqaalabr@uq.edu.au>
2010-11-23 06:15:36 +0000

    changed integration tests to use HTTPClient instead of abdera client (which is shit)
    git-svn-id: https://source.eait.uq.edu.au/svn/maenad-data-registry/trunk@239 a06b0660-b68b-0410-9a37-e6c9a29c9ca9

--- data-registry-test/src/test/java/net/metadata/dataspace/atom/adapter/ServiceTest.java
+++ data-registry-test/src/test/java/net/metadata/dataspace/atom/adapter/ServiceTest.java
@@ -3,8 +3,11 @@ package net.metadata.dataspace.atom.adapter;
 import net.metadata.dataspace.app.Constants;
 import net.metadata.dataspace.app.DataCollectionsRegistryTestCase;
 import net.metadata.dataspace.atom.util.TestHelper;
-import org.apache.abdera.protocol.Response;
-import org.apache.abdera.protocol.client.AbderaClient;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.DeleteMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.PutMethod;
 
 /**
  * Author: alabri
@@ -14,25 +17,41 @@ import org.apache.abdera.protocol.client.AbderaClient;
 public class ServiceTest extends DataCollectionsRegistryTestCase {
 
     public void testServiceCRUD() throws Exception {
-        AbderaClient client = TestHelper.login(Constants.USERNAME, Constants.PASSWORD);
+        //create a client
+        HttpClient client = new HttpClient();
+        //authenticate
+        int status = TestHelper.login(client, Constants.USERNAME, Constants.PASSWORD);
+        assertEquals("Could not authenticate", 200, status);
         //Post Entry
         String fileName = "/files/post/new-service.xml";
-        Response response = TestHelper.postEntry(client, fileName, Constants.PATH_FOR_SERVICES);
-        assertEquals("Could not post entry", 201, response.getStatus());
-        String newEntryLocation = response.getLocation().toString();
-        response = TestHelper.getEntry(client, newEntryLocation);
-        assertEquals("Could not get entry after post", 200, response.getStatus());
+        PostMethod postMethod = TestHelper.postEntry(client, fileName, Constants.PATH_FOR_SERVICES);
+        assertEquals("Could not post entry", 201, postMethod.getStatusCode());
+        String newEntryLocation = postMethod.getResponseHeader("Location").getValue();
+        //Get entry
+        GetMethod getMethod = TestHelper.getEntry(client, newEntryLocation, Constants.ATOM_ENTRY_MIMETYPE);
+        assertEquals("Could not get entry after post", 200, getMethod.getStatusCode());
+        //get first version
+        getMethod = TestHelper.getEntry(client, newEntryLocation + "/1", Constants.ATOM_ENTRY_MIMETYPE);
+        assertEquals("Could not get first version of entry after post", 200, getMethod.getStatusCode());
+        //get working copy
+        getMethod = TestHelper.getEntry(client, newEntryLocation + "/working-copy", Constants.ATOM_ENTRY_MIMETYPE);
+        assertEquals("Could not get working copy after post", 200, getMethod.getStatusCode());
         //Edit Entry
         fileName = "/files/put/update-service.xml";
-        response = TestHelper.putEntry(client, fileName, newEntryLocation);
-        assertEquals("Could not edit entry", 200, response.getStatus());
-        response = TestHelper.getEntry(client, newEntryLocation + "/2");
-        assertEquals("Could not get second version of entry after edit", 200, response.getStatus());
+        PutMethod putMethod = TestHelper.putEntry(client, fileName, newEntryLocation, Constants.ATOM_ENTRY_MIMETYPE);
+        assertEquals("Could not edit entry", 200, putMethod.getStatusCode());
+        //get second version
+        getMethod = TestHelper.getEntry(client, newEntryLocation + "/2", Constants.ATOM_ENTRY_MIMETYPE);
+        assertEquals("Could not get second version of entry after edit", 200, getMethod.getStatusCode());
+        //Get version history
+        getMethod = TestHelper.getEntry(client, newEntryLocation + "/version-history", Constants.ATOM_ENTRY_MIMETYPE);
+        assertEquals("Could not get working copy after post", 200, getMethod.getStatusCode());
         //Delete Entry
-        response = TestHelper.deleteEntry(client, newEntryLocation);
-        assertEquals("Could not delete entry", 200, response.getStatus());
-        response = TestHelper.getEntry(client, newEntryLocation);
-        assertEquals("Entry should be GONE", 410, response.getStatus());
+        DeleteMethod deleteMethod = TestHelper.deleteEntry(client, newEntryLocation);
+        assertEquals("Could not delete entry", 200, deleteMethod.getStatusCode());
+        //check that entry is deleted
+        getMethod = TestHelper.getEntry(client, newEntryLocation, Constants.ATOM_ENTRY_MIMETYPE);
+        assertEquals("Entry should be GONE", 410, getMethod.getStatusCode());
     }
 }
 
--- data-registry-test/src/test/java/net/metadata/dataspace/atom/util/TestHelper.java
+++ data-registry-test/src/test/java/net/metadata/dataspace/atom/util/TestHelper.java
@@ -3,15 +3,10 @@ package net.metadata.dataspace.atom.util;
 import net.metadata.dataspace.app.Constants;
 import org.apache.abdera.Abdera;
 import org.apache.abdera.examples.xsltxpath.XPathExample;
-import org.apache.abdera.model.Document;
-import org.apache.abdera.model.Entry;
-import org.apache.abdera.parser.Parser;
-import org.apache.abdera.protocol.Response;
-import org.apache.abdera.protocol.client.AbderaClient;
-import org.apache.abdera.protocol.client.ClientResponse;
-import org.apache.abdera.protocol.client.RequestOptions;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.*;
 
-import java.io.InputStream;
+import java.io.File;
 
 /**
  * Author: alabri
@@ -19,52 +14,45 @@ import java.io.InputStream;
  * Time: 4:06:52 PM
  */
 public class TestHelper {
+    private static Abdera abdera = new Abdera();
 
-    public static AbderaClient login(String username, String password) throws Exception {
-        Abdera abdera = new Abdera();
-        AbderaClient client = new AbderaClient(abdera);
-        client.get(Constants.URL_PREFIX + "login?username=" + username + "&password=" + password);
-        return client;
+    public static int login(HttpClient client, String username, String password) throws Exception {
+        PostMethod postMethod = new PostMethod(Constants.URL_PREFIX + "login?username=" + username + "&password=" + password);
+        return client.executeMethod(postMethod);
     }
 
-    public static Response postEntry(AbderaClient abderaClient, String fileName, String pathForActivities) {
-        Abdera abdera = new Abdera();
-        InputStream in = XPathExample.class.getResourceAsStream(fileName);
-        Parser parser = abdera.getParser();
-        Document doc = parser.parse(in);
-        Entry entry = (Entry) doc.getRoot();
-        RequestOptions options = abderaClient.getDefaultRequestOptions();
-
-        options.setUseChunked(false);
+    public static PostMethod postEntry(HttpClient client, String fileName, String pathForActivities) throws Exception {
         String fullURL = Constants.URL_PREFIX + pathForActivities;
-        Response response = abderaClient.post(fullURL, entry, options);
-        return response;
+        PostMethod post = new PostMethod(fullURL);
+        String path = XPathExample.class.getResource(fileName).getFile();
+        File inputFile = new File(path);
+        RequestEntity entity = new FileRequestEntity(inputFile, Constants.ATOM_ENTRY_MIMETYPE);
+        post.setRequestEntity(entity);
+        client.executeMethod(post);
+        return post;
     }
 
-    public static Response putEntry(AbderaClient abderaClient, String fileName, String uri) {
-        Abdera abdera = new Abdera();
-        InputStream in = XPathExample.class.getResourceAsStream(fileName);
-        Parser parser = abdera.getParser();
-        Document doc = parser.parse(in);
-        Entry entry = (Entry) doc.getRoot();
-        RequestOptions options = abderaClient.getDefaultRequestOptions();
-        options.setUseChunked(false);
-        Response response = abderaClient.put(uri, entry, options);
-        return response;
+    public static PutMethod putEntry(HttpClient client, String fileName, String uri, String acceptHeader) throws Exception {
+        PutMethod putMethod = new PutMethod(uri);
+        putMethod.setRequestHeader("Accept", acceptHeader);
+        String path = XPathExample.class.getResource(fileName).getFile();
+        File inputFile = new File(path);
+        RequestEntity entity = new FileRequestEntity(inputFile, Constants.ATOM_ENTRY_MIMETYPE);
+        putMethod.setRequestEntity(entity);
+        client.executeMethod(putMethod);
+        return putMethod;
     }
 
-    public static ClientResponse getEntry(AbderaClient abderaClient, String uri) {
-//        Abdera abdera = new Abdera();
-//        AbderaClient abderaClient = new AbderaClient(abdera);
-        ClientResponse clientResponse = abderaClient.get(uri);
-        return clientResponse;
+    public static GetMethod getEntry(HttpClient client, String uri, String acceptHeader) throws Exception {
+        GetMethod getMethod = new GetMethod(uri);
+        getMethod.setRequestHeader("Accept", acceptHeader);
+        client.executeMethod(getMethod);
+        return getMethod;
     }
 
-    public static ClientResponse deleteEntry(AbderaClient abderaClient, String uri) {
-//        Abdera abdera = new Abdera();
-        ClientResponse clientResponse = abderaClient.delete(uri);
-        return clientResponse;
+    public static DeleteMethod deleteEntry(HttpClient client, String uri) throws Exception {
+        DeleteMethod deleteMethod = new DeleteMethod(uri);
+        client.executeMethod(deleteMethod);
+        return deleteMethod;
     }
-
-
 }
