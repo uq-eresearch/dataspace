d03c788831e45c1b4ff1c641c551c5dc3e450036
Abdul Alabri <uqaalabr@uq.edu.au>
2011-02-04 06:37:49 +0000

    added oaiproperties so to make it work with spring
    git-svn-id: https://source.eait.uq.edu.au/svn/maenad-data-registry/trunk@330 a06b0660-b68b-0410-9a37-e6c9a29c9ca9

---
+++ data-registry-webapp/src/main/java/net/metadata/dataspace/oaipmh/OAIProperties.java
@@ -0,0 +1,38 @@
+package net.metadata.dataspace.oaipmh;
+
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Author: alabri
+ * Date: 04/02/2011
+ * Time: 3:30:36 PM
+ */
+public class OAIProperties extends Properties {
+    private Logger logger = Logger.getLogger(getClass());
+
+    public OAIProperties(String propertiesFile) {
+        InputStream resourceAsStream = null;
+        try {
+            resourceAsStream = OAIProperties.class.getResourceAsStream(propertiesFile);
+            if (resourceAsStream == null) {
+                logger.fatal("Configuration file not found, please ensure there is a " + propertiesFile + " on the classpath");
+            }
+            load(resourceAsStream);
+        } catch (IOException ex) {
+            logger.fatal("Failed to load configuration properties", ex);
+        } finally {
+            if (resourceAsStream != null) {
+                try {
+                    resourceAsStream.close();
+                } catch (IOException ex) {
+                    // so what?
+                }
+            }
+        }
+
+    }
+}
