d8750021ba084afbe6ebf0e6338e78b2af8f84e6
Abdul Alabri <uqaalabr@uq.edu.au>
2011-02-14 05:50:07 +0000

    added entity types
    git-svn-id: https://source.eait.uq.edu.au/svn/maenad-data-registry/trunk@379 a06b0660-b68b-0410-9a37-e6c9a29c9ca9

---
+++ data-registry-webapp/src/main/java/net/metadata/dataspace/data/model/types/ActivityType.java
@@ -0,0 +1,11 @@
+package net.metadata.dataspace.data.model.types;
+
+/**
+ * Author: alabri
+ * Date: 14/02/2011
+ * Time: 3:46:37 PM
+ */
+public enum ActivityType {
+    PROJECT,
+    PROGRAM
+}
---
+++ data-registry-webapp/src/main/java/net/metadata/dataspace/data/model/types/ServiceType.java
@@ -0,0 +1,18 @@
+package net.metadata.dataspace.data.model.types;
+
+/**
+ * Author: alabri
+ * Date: 14/02/2011
+ * Time: 3:47:00 PM
+ */
+public enum ServiceType {
+    CREATE,
+    GENERATE,
+    REPORT,
+    ANNOTATE,
+    TRANSFORM,
+    ASSEMBLE,
+    HARVEST,
+    SEARCH,
+    SYNDICATE
+}
