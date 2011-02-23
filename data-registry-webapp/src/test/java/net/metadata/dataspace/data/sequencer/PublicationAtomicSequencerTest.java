6a0160dfd181850b2b334180a148cfec064f9fe0
Abdul Alabri <uqaalabr@uq.edu.au>
2011-02-23 06:46:30 +0000

    added unit tests for pulication and source sequencers
    git-svn-id: https://source.eait.uq.edu.au/svn/maenad-data-registry/trunk@402 a06b0660-b68b-0410-9a37-e6c9a29c9ca9

---
+++ data-registry-webapp/src/test/java/net/metadata/dataspace/data/sequencer/PublicationAtomicSequencerTest.java
@@ -0,0 +1,44 @@
+package net.metadata.dataspace.data.sequencer;
+
+import net.metadata.dataspace.app.NonProductionConstants;
+import net.metadata.dataspace.data.access.manager.EntityCreator;
+import net.metadata.dataspace.data.model.context.Subject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Author: alabri
+ * Date: 23/02/2011
+ * Time: 4:45:03 PM
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = NonProductionConstants.TEST_CONTEXT)
+public class PublicationAtomicSequencerTest {
+
+    @Autowired
+    private PublicationSequencer publicationSequencer;
+
+    @Autowired
+    private EntityCreator entityCreaotr;
+
+    @Test
+    public void testNext() throws Exception {
+        int currentNumber = publicationSequencer.current();
+        assertTrue("Current number should be smaller than next number.", currentNumber < publicationSequencer.next());
+    }
+
+    @Test
+    public void testSubjectSequencing() throws Exception {
+        Subject subject1 = entityCreaotr.getNextSubject();
+        Subject subject2 = entityCreaotr.getNextSubject();
+        Subject subject3 = entityCreaotr.getNextSubject();
+        assertTrue("Atomic number should increase by 1.", subject1.getAtomicNumber() + 1 == subject2.getAtomicNumber());
+        assertTrue("Atomic number should increase by 2.", subject1.getAtomicNumber() + 2 == subject3.getAtomicNumber());
+        assertTrue("Atomic number should increase by 1.", subject2.getAtomicNumber() + 1 == subject3.getAtomicNumber());
+    }
+}
