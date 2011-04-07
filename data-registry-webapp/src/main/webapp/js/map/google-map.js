0fcd34de9030324f34dff7443bd9f8b55aa7946c
Abdul Alabri <uqaalabr@uq.edu.au>
2011-04-07 01:23:37 +0000

    made open street maps as default
    git-svn-id: https://source.eait.uq.edu.au/svn/maenad-data-registry/trunk@497 a06b0660-b68b-0410-9a37-e6c9a29c9ca9

--- data-registry-webapp/src/main/webapp/js/map/google-map.js
+++ data-registry-webapp/src/main/webapp/js/map/google-map.js
@@ -1,30 +1,29 @@
 var map, drawControls;
+
 function init() {
+
     map = new OpenLayers.Map('map');
-
+    var osm = new OpenLayers.Layer.OSM();
     var wmsLayer = new OpenLayers.Layer.WMS("OpenLayers WMS", "http://vmap0.tiles.osgeo.org/wms/vmap0?", {layers: 'basic'});
-
     var pointLayer = new OpenLayers.Layer.Vector("Point Layer");
     var polygonLayer = new OpenLayers.Layer.Vector("Polygon Layer");
-
-    map.addLayers([wmsLayer, pointLayer, polygonLayer]);
-    map.addControl(new OpenLayers.Control.LayerSwitcher());
-    map.addControl(new OpenLayers.Control.MousePosition());
-
+    map.addLayers([osm, wmsLayer, pointLayer, polygonLayer]);
     drawControls = {
         point: new OpenLayers.Control.DrawFeature(pointLayer,
                 OpenLayers.Handler.Point),
         polygon: new OpenLayers.Control.DrawFeature(polygonLayer,
                 OpenLayers.Handler.Polygon)
     };
-
     for (var key in drawControls) {
         map.addControl(drawControls[key]);
     }
 
-    map.setCenter(new OpenLayers.LonLat(0, 0), 3);
 
+    map.setCenter(new OpenLayers.LonLat(0, 0), 3);
+    map.addControl(new OpenLayers.Control.LayerSwitcher());
+    map.addControl(new OpenLayers.Control.MousePosition());
     document.getElementById('noneToggle').checked = true;
+
 }
 
 function toggleControl(element) {
